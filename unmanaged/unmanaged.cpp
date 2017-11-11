e9a69ce2edfad9f2596f5cb5ad073ada8ccabb56
Chijun Sima <1300339379@qq.com>
2017-11-11 17:35:49 +0800

    [Sync] 5

---
+++ unmanaged/unmanaged.cpp
@@ -0,0 +1,5 @@
+﻿#include "pch.h"
+#include "unmanaged.h"
+
+using namespace unmanaged;
+using namespace Platform;
---
+++ unmanaged/unmanaged.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+#include"pch.h"
+#include"wrapper.h"
+using namespace Platform;
+
+namespace unmanaged
+{
+	public ref class cliwrapper sealed
+	{
+	public:
+		cliwrapper() { }
+		Array<int>^ Putcomputer(bool hard, int turn)
+		{
+			std::tuple<int, int8_t, int8_t> x = ptr->Putcomputer(hard, turn);
+			auto res = ref new Array<int>(3);
+			res[0] = std::get<0>(x);
+			res[1] = std::get<1>(x);
+			res[2] = std::get<2>(x);
+			return res;
+		}
+		bool Fullboard()
+		{
+			return ptr->Fullboard();
+		}
+		bool BoardUndo()
+		{
+			return ptr->Undoboard();
+		}
+		int Checkwin(bool change)
+		{
+			return ptr->Checkwin(change);
+		}
+		bool Puthuman(int row, int col, int turn)
+		{
+			return ptr->Put(row, col, turn);
+		}
+		Array<int>^ Getchessboard()
+		{
+			auto res = ref new Array<int>(15 * 15);
+			for (int i = 0; i < 15; i++)
+				for (int ii = 0; ii < 15; ii++)
+					res[i * 15 + ii] = ptr->Getchessboard(static_cast<int8_t>(i), static_cast<int8_t>(ii));
+			return res;
+		}
+		Array<int>^ Gethistory()
+		{
+			auto x = ptr->Gethistory();
+			auto res = ref new Array<int>(x.size() * 3);
+			for (int i = 0; i < x.size(); i++)
+			{
+				res[3 * i] = std::get<0>(x[i]);
+				res[3 * i + 1] = std::get<1>(x[i]);
+				res[3 * i + 2] = std::get<2>(x[i]);
+			}
+			return res;
+		}
+	private:
+		std::unique_ptr<Cwrapper>ptr = std::make_unique<Cwrapper>();
+	};
+}
