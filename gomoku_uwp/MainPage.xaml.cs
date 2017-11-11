629fbb2b44b7ec39df98994027fc493496ade970
Chijun Sima <1300339379@qq.com>
2017-11-11 17:52:21 +0800

    [UI]

--- gomoku_uwp/MainPage.xaml.cs
+++ gomoku_uwp/MainPage.xaml.cs
@@ -12,7 +12,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using unmanaged;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
