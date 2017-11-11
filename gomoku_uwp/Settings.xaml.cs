028863ce649bbf5f690677f1b5ed35240f59adc5
Chijun Sima <1300339379@qq.com>
2017-11-11 22:42:20 +0800

    [Add] settings

--- gomoku_uwp/Settings.xaml.cs
+++ gomoku_uwp/Settings.xaml.cs
@@ -22,9 +22,84 @@ namespace gomoku_uwp
     /// </summary>
     public sealed partial class Settings : Page
     {
+        Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
         public Settings()
         {
             this.InitializeComponent();
+            initSettings();
+        }
+        public void initSettings()
+        {
+            Object valueMode = localSettings.Values["mode"];
+            Object valueBlack = localSettings.Values["black"];
+            Object valueWhite = localSettings.Values["white"];
+            if (valueMode == null||valueBlack==null||valueWhite==null)
+            {
+                localSettings.Values["mode"] = "hard";
+                localSettings.Values["black"] = "computer";
+                localSettings.Values["white"] = "human";
+            }
+            else
+            {
+                switch(localSettings.Values["mode"])
+                {
+                    case "hard":
+                        Mode_Hard.IsChecked = true;
+                        break;
+                    case "easy":
+                        Mode_Easy.IsChecked = true;
+                        break;
+                }
+                switch (localSettings.Values["black"])
+                {
+                    case "computer":
+                        Black_Computer.IsChecked = true;
+                        break;
+                    case "human":
+                        Black_Human.IsChecked = true;
+                        break;
+                }
+                switch (localSettings.Values["white"])
+                {
+                    case "computer":
+                        White_Computer.IsChecked = true;
+                        break;
+                    case "human":
+                        White_Human.IsChecked = true;
+                        break;
+                }
+            }
+        }
+
+        private void B_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb != null)
+            {
+                string Name = rb.Tag.ToString();
+                switch (Name)
+                {
+                    case "Hard":
+                        localSettings.Values["mode"] = "hard";
+
+                        break;
+                    case "Easy":
+                        localSettings.Values["mode"] = "easy";
+                        break;
+                    case "ComputerWhite":
+                        localSettings.Values["white"] = "computer";
+                        break;
+                    case "HumanWhite":
+                        localSettings.Values["white"] = "human";
+                        break;
+                    case "ComputerBlack":
+                        localSettings.Values["black"] = "computer";
+                        break;
+                    case "HumanBlack":
+                        localSettings.Values["black"] = "human";
+                        break;
+                }
+            }
         }
     }
 }
