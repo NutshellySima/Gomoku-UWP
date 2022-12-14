242a485eb95c525ab8e0d27e605d8ea488a7ff78
Chijun Sima <simachijun@gmail.com>
2022-12-14 14:36:13 +0800

    dark mode

--- gomoku/MainWindow.xaml.cs
+++ gomoku/MainWindow.xaml.cs
@@ -45,8 +45,43 @@ namespace gomoku
 
             SetTitleBarIcon();
 
+            UseImmersiveDarkMode(true);
+
             Title = AppInfo.Current.DisplayInfo.DisplayName;
             rootFrame.Navigate(typeof(Home));
+        }
+
+        /*
+            using System.Runtime.InteropServices;
+        */
+
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        private bool UseImmersiveDarkMode(bool enabled)
+        {
+            IntPtr handle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            if (IsWindows10OrGreater(17763))
+            {
+                var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+                if (IsWindows10OrGreater(18985))
+                {
+                    attribute = DWMWA_USE_IMMERSIVE_DARK_MODE;
+                }
+
+                int useImmersiveDarkMode = enabled ? 1 : 0;
+                return DwmSetWindowAttribute(handle, (int)attribute, ref useImmersiveDarkMode, sizeof(int)) == 0;
+            }
+
+            return false;
+        }
+
+        private static bool IsWindows10OrGreater(int build = -1)
+        {
+            return Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Build >= build;
         }
 
         private void SetTitleBarIcon()
